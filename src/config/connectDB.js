0839dca04894eb088d2c719d90317d8bf5d62814
devanshugahlot <devanshugahlot79@gmail.com>
2024-09-25 22:16:53 +0530

    frist

---
+++ src/config/connectDB.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql2/promise');
+
+const connection = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'damantwo'
+});
+
+// const connection = mysql.createPool({
+//     host: 'localhost',
+//     user: 'admin',
+//     password: 'Dph51mO5qkS8U1k',
+//     database: '92lottery'
+// });
+
+export default connection;
